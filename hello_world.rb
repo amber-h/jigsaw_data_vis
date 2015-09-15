7ffb06eadefc5bd81d7b35a37ac0f337d5b07aa0
Amber Houle <amber.houle3@gmail.com>
2015-09-15 12:38:25 -0500

    Amber: Adding jigsaw people data to index.haml, displayed in json format

--- hello_world.rb
+++ hello_world.rb
@@ -2,8 +2,8 @@ require 'sinatra'
 require 'rest_client'
 
 get '/' do
-   jigsaw_data = RestClient::Resource.new('https://jigsaw.thoughtworks.com/api', headers: {:Authorization => 'ENV[JIGSAW_API_TOKEN]'})
-   puts jigsaw_data["people"].get
-
+   @jigsaw_data = RestClient::Resource.new('https://jigsaw.thoughtworks.com/api', headers: {:Authorization => 'ENV[JIGSAW_API_TOKEN]'})
+   @jigsaw_people = @jigsaw_data["people"].get
+   puts @jigsaw_people
    haml :index, :format => :html5
 end
