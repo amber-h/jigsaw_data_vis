9679e6c68b5e7592472c13d4e597044128c8bd02
Amber Houle <amber.houle3@gmail.com>
2015-09-14 21:13:24 -0500

    Amber: added Gemfile, and added RestClient get request to jigsaw (commented out)

--- hello_world.rb
+++ hello_world.rb
@@ -1,7 +1,7 @@
-require 'rubygems'
-require 'bundler/setup'
 require 'sinatra'
+require 'rest_client'
 
 get '/' do
+   #data = RestClient.get 'https://jigsaw.thoughtworks.com/api/people', {:Authorization => 'auth token goes here'}
    haml :index, :format => :html5
 end
