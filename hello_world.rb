348f2376ebd361650316fcc4e5cf9f3abf3b5227
Amber Houle <amber.houle3@gmail.com>
2015-09-15 12:11:07 -0500

    Amber: Changed rest-client to use a resourceto successfully retrieve jigsaw people data from get request

--- hello_world.rb
+++ hello_world.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'rest_client'
 
 get '/' do
-   #data = RestClient.get 'https://jigsaw.thoughtworks.com/api/people', {:Authorization => 'auth token goes here'}
+   jigsaw_data = RestClient::Resource.new('https://jigsaw.thoughtworks.com/api', headers: {:Authorization => 'ENV[JIGSAW_API_TOKEN]'})
+   puts jigsaw_data["people"].get
+
    haml :index, :format => :html5
 end
